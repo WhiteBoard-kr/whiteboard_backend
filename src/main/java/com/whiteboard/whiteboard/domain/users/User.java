c64ec1666f9c99490ed36827de8f870283905f85
dreamjh20 <dreamjh20@gmail.com>
2022-04-04 12:07:38 +0900

    ADD: User's Id, Name, Password id as pk

--- src/main/java/com/whiteboard/whiteboard/domain/users/User.java
+++ src/main/java/com/whiteboard/whiteboard/domain/users/User.java
@@ -1,4 +1,25 @@
 package com.whiteboard.whiteboard.domain.users;
 
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 30, nullable = false)
+    private String name;
+
+    @Column(length = 50, nullable = false)
+    private String password;
+
 }
