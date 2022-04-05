ee3297d46e1a606a7b011cfa5e559270904e57de
dreamjh20 <dreamjh20@gmail.com>
2022-04-05 17:16:52 +0900

    ADD: Divide Requests HttpSecurity

--- src/main/java/com/whiteboard/whiteboard/security/WebSecurityConfig.java
+++ src/main/java/com/whiteboard/whiteboard/security/WebSecurityConfig.java
@@ -1,5 +1,20 @@
 package com.whiteboard.whiteboard.security;
 
-public class WebSecurityConfig {
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //요청 가로채기
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic()
+                .and()
+                .authorizeHttpRequests()
+                //Ant 스타일 와일드카드
+                .antMatchers("/user/**").hasRole("USER");
+    }
 
 }
