86364d41339a1f94604891da21409a833e44f638
dreamjh20 <dreamjh20@gmail.com>
2022-04-05 17:13:28 +0900

    ADD: Secret Key environment variable

--- src/main/java/com/whiteboard/whiteboard/security/JwtTokenProvider.java
+++ src/main/java/com/whiteboard/whiteboard/security/JwtTokenProvider.java
@@ -1,4 +1,16 @@
 package com.whiteboard.whiteboard.security;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+//생성자 주입
+@RequiredArgsConstructor
+@Component
 public class JwtTokenProvider {
+
+    //암호화 키 환경변수로 저장
+    @Value("${jwt.secret}")
+    private String secretKey;
 }
