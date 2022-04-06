9c3f2da228d6a6fb22f0fa344cfff1bcdfba9b8e
dreamjh20 <dreamjh20@gmail.com>
2022-04-06 15:58:32 +0900

    SET: Id Type Long -> Int

--- src/main/java/com/whiteboard/whiteboard/domain/users/User.java
+++ src/main/java/com/whiteboard/whiteboard/domain/users/User.java
@@ -14,7 +14,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private int id;
 
     @Column(length = 30, nullable = false)
     private String name;
