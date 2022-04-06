7b581debdd10effce561ee025a050d4d42191e84
dreamjh20 <dreamjh20@gmail.com>
2022-04-06 23:53:50 +0900

    ADD: doFilter get jwt from header check jwt available get user info from jwt

--- src/main/java/com/whiteboard/whiteboard/security/JwtAuthenticationFilter.java
+++ src/main/java/com/whiteboard/whiteboard/security/JwtAuthenticationFilter.java
@@ -1,7 +1,35 @@
 package com.whiteboard.whiteboard.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.servlet.function.ServerRequest;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 @RequiredArgsConstructor
-public class JwtAuthenticationFilter {
+public class JwtAuthenticationFilter extends GenericFilterBean {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        //해더에서 jwt 획득
+        String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
+        //jwt 유효성 검사
+        if (token != null && jwtTokenProvider.validateToken(token)) {
+            //사용자 정보 조회
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        chain.doFilter(request, response);
+
+    }
 }
