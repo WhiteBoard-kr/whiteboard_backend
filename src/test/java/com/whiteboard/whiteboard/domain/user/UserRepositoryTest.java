283033c80d52833093a045fd6559aabf27d3f936
dreamjh20 <dreamjh20@gmail.com>
2022-04-06 16:03:17 +0900

    ADD: Test Variables

--- src/test/java/com/whiteboard/whiteboard/domain/user/UserRepositoryTest.java
+++ src/test/java/com/whiteboard/whiteboard/domain/user/UserRepositoryTest.java
@@ -1,4 +1,36 @@
 package com.whiteboard.whiteboard.domain.user;
 
+import com.whiteboard.whiteboard.WhiteboardApplication;
+import com.whiteboard.whiteboard.domain.users.UserRepository;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = WhiteboardApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class UserRepositoryTest {
+
+    @Autowired
+    UserRepository userRepository;
+
+    //테스트 후
+    @After
+    public void cleanup() {
+        //데이터배이스 초기화
+        userRepository.deleteAll();
+    }
+
+    //테스트
+    @Test
+    public void call_regist_user() {
+        int id = 999;
+        String name = "Test Name";
+        String password = "Test Password";
+
+    }
+
+
 }
