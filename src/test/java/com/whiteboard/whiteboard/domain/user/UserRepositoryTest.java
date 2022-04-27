7a0b386a2c843e0ad9c5f3ec62856e3e8bc58aba
dreamjh20 <dreamjh20@gmail.com>
2022-04-27 09:57:54 +0900

    FEAT: Registng Test

--- src/test/java/com/whiteboard/whiteboard/domain/user/UserRepositoryTest.java
+++ src/test/java/com/whiteboard/whiteboard/domain/user/UserRepositoryTest.java
@@ -1,15 +1,20 @@
 package com.whiteboard.whiteboard.domain.user;
 
 import com.whiteboard.whiteboard.WhiteboardApplication;
+import com.whiteboard.whiteboard.domain.users.User;
 import com.whiteboard.whiteboard.domain.users.UserRepository;
-import org.junit.After;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import java.util.Optional;
 
-@RunWith(SpringRunner.class)
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = WhiteboardApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class UserRepositoryTest {
 
@@ -17,7 +22,7 @@ public class UserRepositoryTest {
     UserRepository userRepository;
 
     //테스트 후
-    @After
+    @AfterEach
     public void cleanup() {
         //데이터배이스 초기화
         userRepository.deleteAll();
@@ -25,12 +30,22 @@ public class UserRepositoryTest {
 
     //테스트
     @Test
-    public void call_regist_user() {
-        int id = 999;
+    public void register_user() {
+
         String name = "Test Name";
         String password = "Test Password";
 
-    }
+        userRepository.save(User.builder()
+                .name(name)
+                .password(password)
+                .build());
 
+        Optional<User> resultUser = userRepository.findByName(name);
+        assertTrue(resultUser.isPresent());
+
+        User user = resultUser.get();
+        assertEquals(name, user.getName());
+        assertEquals(password, user.getPassword());
+    }
 
 }
