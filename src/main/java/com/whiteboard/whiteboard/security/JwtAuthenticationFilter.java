b851aadf8d766b27926ea01a638e11d2bb00ba4d
dreamjh20 <dreamjh20@gmail.com>
2022-04-27 09:50:30 +0900

    ADD: Filterchain

--- src/main/java/com/whiteboard/whiteboard/security/JwtAuthenticationFilter.java
+++ src/main/java/com/whiteboard/whiteboard/security/JwtAuthenticationFilter.java
@@ -3,33 +3,29 @@ package com.whiteboard.whiteboard.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.GenericFilterBean;
-import org.springframework.web.servlet.function.ServerRequest;
-import org.springframework.web.servlet.function.ServerResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @RequiredArgsConstructor
-public class JwtAuthenticationFilter extends GenericFilterBean {
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtTokenProvider jwtTokenProvider;
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         //해더에서 jwt 획득
-        String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
+        String token = jwtTokenProvider.resolveToken(request);
         //jwt 유효성 검사
         if (token != null && jwtTokenProvider.validateToken(token)) {
             //사용자 정보 조회
             Authentication authentication = jwtTokenProvider.getAuthentication(token);
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
-        chain.doFilter(request, response);
-
+        filterChain.doFilter(request, response);
     }
 }
