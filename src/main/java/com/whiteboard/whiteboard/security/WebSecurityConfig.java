9cf6ca60df61fa52173e8352de0fee4a03da2286
dreamjh20 <dreamjh20@gmail.com>
2022-04-27 09:58:17 +0900

    FEAT: Registng Test

--- src/main/java/com/whiteboard/whiteboard/security/WebSecurityConfig.java
+++ src/main/java/com/whiteboard/whiteboard/security/WebSecurityConfig.java
@@ -1,20 +1,48 @@
 package com.whiteboard.whiteboard.security;
 
+import com.whiteboard.whiteboard.domain.users.RoleType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Configuration
+@RequiredArgsConstructor
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    //패스워드 암호화
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
     //요청 가로채기
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic()
+        http
+                .csrf().disable()
+                .formLogin().disable()
+                .cors()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeHttpRequests()
                 //Ant 스타일 와일드카드
-                .antMatchers("/user/**").hasRole("USER");
+                .antMatchers("/user").permitAll()
+                .antMatchers("/photo").hasRole(RoleType.ADMIN.toString())
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
+                        UsernamePasswordAuthenticationFilter.class);
     }
 
 }
