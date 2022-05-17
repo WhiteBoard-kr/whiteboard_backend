7ddb0550031eca48ed691a743ba9953ea27df24f
dreamjh20 <dreamjh20@gmail.com>
2022-05-17 09:47:05 +0900

    UPDATE: Column Name

--- src/main/java/com/whiteboard/whiteboard/domain/photos/Photo.java
+++ src/main/java/com/whiteboard/whiteboard/domain/photos/Photo.java
@@ -1,4 +1,9 @@
 package com.whiteboard.whiteboard.domain.photos;
 
+import javax.persistence.Column;
+
 public class Photo {
+    
+    @Column(length = 250, nullable = true)
+    private String photo_url;
 }
