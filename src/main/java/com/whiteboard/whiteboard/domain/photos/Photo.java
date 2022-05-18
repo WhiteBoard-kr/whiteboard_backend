3021a1799f53f2eaf0ae52afc66cc1aa43f56604
dreamjh20 <dreamjh20@gmail.com>
2022-05-18 11:08:19 +0900

    ADD: Foreign Key User

--- src/main/java/com/whiteboard/whiteboard/domain/photos/Photo.java
+++ src/main/java/com/whiteboard/whiteboard/domain/photos/Photo.java
@@ -1,9 +1,33 @@
 package com.whiteboard.whiteboard.domain.photos;
 
-import javax.persistence.Column;
+import com.whiteboard.whiteboard.domain.users.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Table(name = "PHOTO")
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Photo {
-    
-    @Column(length = 250, nullable = true)
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long liked_id;
+
+    //User Id
+    @ManyToOne
+    @JoinColumn(name = "id")
+    private User user;
+
+    @Column(length = 100)
+    private long photo_id;
+
+    @Column(length = 250)
     private String photo_url;
 }
