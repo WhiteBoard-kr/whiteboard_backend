2d6f965ffb88269553914aa22756eff4c3203f5a
dreamjh20 <dreamjh20@gmail.com>
2022-06-02 19:42:05 +0900

    ADD: Example Apis

--- src/main/java/com/whiteboard/whiteboard/web/user/PhotoController.java
+++ src/main/java/com/whiteboard/whiteboard/web/user/PhotoController.java
@@ -1,9 +1,28 @@
 package com.whiteboard.whiteboard.web.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 public class PhotoController {
+
+    @GetMapping("/photo")
+    public String photo() {
+        return "PHOTO";
+    }
+
+    @GetMapping("/photo/liked")
+    public String liked_photo() {
+        return "LIKED PHOTO";
+    }
+
+    @PostMapping("/photo/like")
+    public String like_photo() {
+        return "LIKE PHOTO";
+    }
+
 }
