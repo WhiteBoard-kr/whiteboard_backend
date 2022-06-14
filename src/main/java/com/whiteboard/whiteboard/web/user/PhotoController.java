b0a889542f03ad03a3c75c017c2682a39c084b8a
dreamjh20 <dreamjh20@gmail.com>
2022-06-14 20:15:29 +0900

    ADD: Random Number List

--- src/main/java/com/whiteboard/whiteboard/web/user/PhotoController.java
+++ src/main/java/com/whiteboard/whiteboard/web/user/PhotoController.java
@@ -1,18 +1,44 @@
 package com.whiteboard.whiteboard.web.user;
 
+import com.whiteboard.whiteboard.config.random.GenerateRandom;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
 public class PhotoController {
 
-    @GetMapping("/photo")
-    public String photo() {
-        return "PHOTO";
+    private final GenerateRandom generateRandom;
+    @GetMapping("/photo/{number_of_photos}")
+    public Map photo(@PathVariable("number_of_photos") int number_of_photos) {
+        Map result = new HashMap<String, String>();
+
+        result.put("ID", "id");
+        result.put("PW", "pw");
+        //맵을 두 개 둬서 하나는 인트 배열
+        //이따 해봐 준혁 화이팅!
+        if (number_of_photos > 10000) {
+            result.put("message", "failed");
+            result.put("satus_code", "400");
+
+            return result;
+        }
+        int[] randList = generateRandom.randomList(number_of_photos);
+        String randStr = "";
+        for (int i = 0; i < number_of_photos; i++) {
+            if (i == 0) {
+                randStr += randList[0];
+            }
+            else
+                randStr = randStr + ", " + randList[i];
+        }
+
+        result.put("photo_id", randStr);
+        result.put("status_code", "200");
+        return result;
     }
 
     @GetMapping("/photo/liked")
