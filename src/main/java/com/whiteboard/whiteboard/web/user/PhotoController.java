bfcd16ab8b1c9ebd7ec623120ba2944f289b979b
dreamjh20 <dreamjh20@gmail.com>
2022-06-15 00:11:47 +0900

    ADD: Photo ID

--- src/main/java/com/whiteboard/whiteboard/web/user/PhotoController.java
+++ src/main/java/com/whiteboard/whiteboard/web/user/PhotoController.java
@@ -1,43 +1,34 @@
 package com.whiteboard.whiteboard.web.user;
 
 import com.whiteboard.whiteboard.config.random.GenerateRandom;
-import com.whiteboard.whiteboard.config.random.json.JsonStatus;
+import com.whiteboard.whiteboard.config.random.json.JsonTransducer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
 public class PhotoController {
 
     private final GenerateRandom generateRandom;
+
     @GetMapping("/photo/{number_of_photos}")
     public Object photo(@PathVariable("number_of_photos") int number_of_photos) {
-        JsonStatus jsonStatus = new JsonStatus();
+        JsonTransducer jsonTransducer = new JsonTransducer();
 
-        //맵을 두 개 둬서 하나는 인트 배열
-        //이따 해봐 준혁 화이팅!
         if (number_of_photos >= 10000) {
-            jsonStatus.status_code = "400";
-            jsonStatus.message = "Too Many Photos Required";
+            jsonTransducer.status_code = "400";
+            jsonTransducer.message = "Too Many Photos Required";
 
-            return jsonStatus;
+            return jsonTransducer;
         }
         int[] randList = generateRandom.randomList(number_of_photos);
-        String randStr = "";
-        for (int i = 0; i < number_of_photos; i++) {
-            if (i == 0) {
-                randStr += randList[0];
-            }
-            else
-                randStr = randStr + ", " + randList[i];
-        }
 
-        jsonStatus.status_code = "200";
-        jsonStatus.message = "Success";
-        return jsonStatus;
+        String randStr = "";
+
+        jsonTransducer.status_code = "200";
+        jsonTransducer.message = "Success";
+        jsonTransducer.photo_id = randStr;
+        return jsonTransducer;
     }
 
     @GetMapping("/photo/liked")
