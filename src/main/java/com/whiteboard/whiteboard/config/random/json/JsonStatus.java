08a8cb038835b100a74f572f37ba2bbfed8fbd3b
dreamjh20 <dreamjh20@gmail.com>
2022-06-14 20:29:12 +0900

    CREATE: JsonStatus.java to return json format

---
+++ src/main/java/com/whiteboard/whiteboard/config/random/json/JsonStatus.java
@@ -0,0 +1,11 @@
+package com.whiteboard.whiteboard.config.random.json;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class JsonStatus {
+    public String message;
+    public String status_code;
+}
