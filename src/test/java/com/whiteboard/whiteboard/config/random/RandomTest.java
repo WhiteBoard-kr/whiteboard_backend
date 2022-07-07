f0cf562c99ad1703ebbbfeedf303ae9c1ac27e0e
dreamjh20 <dreamjh20@gmail.com>
2022-07-07 23:55:28 +0900

    DELETE: assertFalse

--- src/test/java/com/whiteboard/whiteboard/config/random/RandomTest.java
+++ src/test/java/com/whiteboard/whiteboard/config/random/RandomTest.java
@@ -11,8 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @RunWith(SpringRunner.class)
